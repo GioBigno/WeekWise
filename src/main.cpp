8df2c8dcc3a1a52fe2828e6712b46ae7809be2f0
giorgio <giorgio.bigno@gmail.com>
2023-11-20 09:52:14 +0100

    resources

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/WeekPlanner/src/main.qml"_qs);
+    const QUrl url(u"qrc:/Main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
         &app, [url](QObject *obj, const QUrl &objUrl) {
             if (!obj && url == objUrl)
