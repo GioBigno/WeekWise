1a97c9ab613e74b7a0f96e8ffb7002b4cda50681
giorgio bignotti <giorgio.bigno@gmail.com>
2023-12-03 18:23:43 +0100

    database schema

--- schema.sql
+++ schema.sql
@@ -8,26 +8,32 @@ CREATE TABLE WeeklySchedule (
     DateEnd TIMESTAMP NOT NULL
 );
 
+CREATE TABLE MacroAreas (
+    MacroAreaID SERIAL PRIMARY KEY,
+    MacroAreaName VARCHAR(255) UNIQUE NOT NULL
+);
+
 CREATE TABLE Activities (
     ActivityID SERIAL PRIMARY KEY,
-    ActivityName VARCHAR(255) NOT NULL
+    MacroAreaID INT NOT NULL,
+    ActivityName VARCHAR(255) UNIQUE NOT NULL,
+    FOREIGN KEY (MacroAreaID) REFERENCES MacroAreas(MacroAreaID)
 );
 
 CREATE TABLE PlannedHours (
     PlannedID SERIAL PRIMARY KEY,
-    ScheduleID INT,
-    ActivityID INT,
+    ScheduleID INT NOT NULL,
+    ActivityID INT NOT NULL,
     PlannedDuration INT NOT NULL,
     FOREIGN KEY (ScheduleID) REFERENCES WeeklySchedule(ScheduleID),
-    FOREIGN KEY (ActivityID) REFERENCES Activities(ActivityID)
+    FOREIGN KEY (ActivityID) REFERENCES Activities(ActivityID),
+    UNIQUE (ScheduleID, ActivityID)
 );
 
 CREATE TABLE LoggedHours (
-    LogID SERIAL PRIMARY KEY,
-    ScheduleID INT,
-    ActivityID INT,
-    ActualDuration INT NOT NULL,
-    DateLogged DATE NOT NULL,
+    ScheduleID INT PRIMARY KEY,
+    ActivityID INT NOT NULL,
+    DateLogged DATE UNIQUE NOT NULL,
     FOREIGN KEY (ScheduleID) REFERENCES WeeklySchedule(ScheduleID),
     FOREIGN KEY (ActivityID) REFERENCES Activities(ActivityID)
 );
