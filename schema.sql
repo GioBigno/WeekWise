ab349cd3ed8366bc35b5ddf5f20440886fd38450
giorgio bignotti <giorgio.bigno@gmail.com>
2023-12-21 21:27:35 +0100

    lower case name

--- schema.sql
+++ schema.sql
@@ -3,39 +3,39 @@ CREATE DATABASE weekwise;
 -- Connect to the weekwise database
 \c weekwise
 
-CREATE TABLE WeeklySchedule (
-    ScheduleID SERIAL PRIMARY KEY,
-    DateStart TIMESTAMP NOT NULL,
-    DateEnd TIMESTAMP NOT NULL
+CREATE TABLE weekly_schedule (
+    schedule_id SERIAL PRIMARY KEY,
+    date_start TIMESTAMP NOT NULL,
+    date_end TIMESTAMP NOT NULL
 );
 
-CREATE TABLE MacroAreas (
-    MacroAreaID SERIAL PRIMARY KEY,
-    MacroAreaName VARCHAR(255) UNIQUE NOT NULL,
-    Color VARCHAR(6) UNIQUE NOT NULL
+CREATE TABLE macroareas (
+    macroarea_id SERIAL PRIMARY KEY,
+    macroarea_name VARCHAR(255) UNIQUE NOT NULL,
+    macroarea_color VARCHAR(6) UNIQUE NOT NULL
 );
 
-CREATE TABLE Activities (
-    ActivityID SERIAL PRIMARY KEY,
-    MacroAreaID INT NOT NULL,
-    ActivityName VARCHAR(255) UNIQUE NOT NULL,
-    FOREIGN KEY (MacroAreaID) REFERENCES MacroAreas(MacroAreaID)
+CREATE TABLE activities (
+    activity_id SERIAL PRIMARY KEY,
+    macroarea_id INT NOT NULL,
+    activity_name VARCHAR(255) UNIQUE NOT NULL,
+    FOREIGN KEY (macroarea_id) REFERENCES macroareas(macroarea_id)
 );
 
-CREATE TABLE PlannedHours (
-    PlannedID SERIAL PRIMARY KEY,
-    ScheduleID INT NOT NULL,
-    ActivityID INT NOT NULL,
-    PlannedDuration INT NOT NULL,
-    FOREIGN KEY (ScheduleID) REFERENCES WeeklySchedule(ScheduleID),
-    FOREIGN KEY (ActivityID) REFERENCES Activities(ActivityID),
-    UNIQUE (ScheduleID, ActivityID)
+CREATE TABLE planned_hours (
+    planned_id SERIAL PRIMARY KEY,
+    schedule_id INT NOT NULL,
+    activity_id INT NOT NULL,
+    planned_duration INT NOT NULL,
+    FOREIGN KEY (schedule_id) REFERENCES weekly_schedule(schedule_id),
+    FOREIGN KEY (activity_id) REFERENCES activities(activity_id),
+    UNIQUE (schedule_id, activity_id)
 );
 
-CREATE TABLE LoggedHours (
-    ScheduleID INT PRIMARY KEY,
-    ActivityID INT NOT NULL,
-    DateLogged DATE UNIQUE NOT NULL,
-    FOREIGN KEY (ScheduleID) REFERENCES WeeklySchedule(ScheduleID),
-    FOREIGN KEY (ActivityID) REFERENCES Activities(ActivityID)
+CREATE TABLE logged_hours (
+    schedule_id INT PRIMARY KEY,
+    activity_id INT NOT NULL,
+    date_logged DATE UNIQUE NOT NULL,
+    FOREIGN KEY (schedule_id) REFERENCES weekly_schedule(schedule_id),
+    FOREIGN KEY (activity_id) REFERENCES activities(activity_id)
 );
