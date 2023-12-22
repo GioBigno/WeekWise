f9aa51e6cccc1cbe5e4736267942de494e389f09
giorgio bignotti <giorgio.bigno@gmail.com>
2023-12-22 23:48:44 +0100

    changed tables

--- schema.sql
+++ schema.sql
@@ -1,36 +1,29 @@
-CREATE TABLE weekly_schedule (
-    schedule_id SERIAL PRIMARY KEY,
-    date_start TIMESTAMP NOT NULL,
-    date_end TIMESTAMP NOT NULL
-);
-
 CREATE TABLE macroareas (
-    macroarea_id SERIAL PRIMARY KEY,
-    macroarea_name VARCHAR(255) UNIQUE NOT NULL,
+    macroarea_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    macroarea_name VARCHAR(30) UNIQUE NOT NULL,
     macroarea_color VARCHAR(6) UNIQUE NOT NULL
 );
 
 CREATE TABLE activities (
-    activity_id SERIAL PRIMARY KEY,
-    macroarea_id INT NOT NULL,
-    activity_name VARCHAR(255) UNIQUE NOT NULL,
+    activity_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    macroarea_id INTEGER NOT NULL,
+    activity_name VARCHAR(30) NOT NULL,
+    UNIQUE (macroarea_id, activity_name),
     FOREIGN KEY (macroarea_id) REFERENCES macroareas(macroarea_id)
 );
 
 CREATE TABLE planned_hours (
-    planned_id SERIAL PRIMARY KEY,
-    schedule_id INT NOT NULL,
-    activity_id INT NOT NULL,
-    planned_duration INT NOT NULL,
-    FOREIGN KEY (schedule_id) REFERENCES weekly_schedule(schedule_id),
+    planned_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    activity_id INTEGER NOT NULL,
+    planned_duration INTEGER NOT NULL,
+    week_date TEXT NOT NULL,
     FOREIGN KEY (activity_id) REFERENCES activities(activity_id),
-    UNIQUE (schedule_id, activity_id)
+    UNIQUE (week_date, activity_id)
 );
 
 CREATE TABLE logged_hours (
-    schedule_id INT PRIMARY KEY,
-    activity_id INT NOT NULL,
-    date_logged TIMESTAMP UNIQUE NOT NULL,
-    FOREIGN KEY (schedule_id) REFERENCES weekly_schedule(schedule_id),
+    logged_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    activity_id INTEGER NOT NULL,
+    date_logged TEXT UNIQUE NOT NULL,
     FOREIGN KEY (activity_id) REFERENCES activities(activity_id)
 );
